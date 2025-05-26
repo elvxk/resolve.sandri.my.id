343975215f5bf36da472848e04abaf5f2042abe2
elvxk <pradiptaramadan9c@gmail.com>
2025-05-26 18:37:17 +0700

    feat(ui): use shadcn

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,8 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Image from "next/image";
 import { useState } from "react";
 
 export default function Home() {
@@ -32,49 +36,58 @@ export default function Home() {
   };
 
   return (
-    <div className="container mx-auto min-h-screen px-4 py-8">
-      <h1 className="text-2xl font-semibold text-center mb-6">RESOLVER</h1>
-
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col md:flex-row gap-4 items-center justify-center mb-8"
-      >
-        <input
-          type="text"
-          placeholder="Host (contoh: word.sandri.my.id)"
-          value={host}
-          onChange={(e) => setHost(e.target.value)}
-          className="border p-2 rounded-md w-full md:w-1/3"
-          required
-        />
-        <input
-          type="text"
-          placeholder="IP (contoh: 103.163.138.33)"
-          value={ip}
-          onChange={(e) => setIp(e.target.value)}
-          className="border p-2 rounded-md w-full md:w-1/3"
-          required
-        />
-        <button
-          type="submit"
-          className="bg-blue-600 text-white px-4 py-2 rounded-md"
-        >
-          {loading ? "Loading..." : "Preview"}
-        </button>
-      </form>
-
-      {resLink && (
-        <div className="flex justify-center">
-          <div className="w-full max-w-[100vw] h-[80vh] shadow-lg border rounded-xl overflow-hidden">
-            <iframe
-              src={resLink}
-              className="w-full h-full"
-              frameBorder="0"
-              allowFullScreen
-            ></iframe>
-          </div>
+    <div className="container mx-auto  px-6 py-8">
+      <div className="flex gap-6 flex-col items-center justify-center min-h-[70vh]">
+        <div className="flex flex-col items-center">
+          <Image
+            src="/resolve.webp"
+            alt="ELVXK TOOL RESOLVER"
+            width={100}
+            height={100}
+          />
+          <h1 className="text-2xl font-bold">RESOLVER PREVIEW</h1>
+          <h1 className="">Skip DNS to preview to an IP</h1>
         </div>
-      )}
+
+        <form onSubmit={handleSubmit} className="w-full flex flex-col gap-4">
+          <div className="flex flex-col md:flex-row gap-4 items-center justify-center">
+            <div className="grid w-full items-center gap-1.5">
+              <Label htmlFor="host">Host</Label>
+              <Input
+                type="text"
+                placeholder="sandri.my.id"
+                value={host}
+                onChange={(e) => setHost(e.target.value)}
+                required
+              />
+            </div>
+            <div className="grid w-full items-center gap-1.5">
+              <Label htmlFor="ip">IP</Label>
+              <Input
+                type="text"
+                placeholder="103.163.138.33"
+                value={ip}
+                onChange={(e) => setIp(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+          <Button type="submit">{loading ? "Loading..." : "Preview"}</Button>
+        </form>
+
+        {resLink && (
+          <div className="w-full max-w-[100vw] h-[80vh] shadow-lg border rounded-xl overflow-hidden">
+            {resLink && (
+              <iframe
+                src={resLink}
+                className="w-full h-full"
+                frameBorder="0"
+                allowFullScreen
+              ></iframe>
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="py-4 opacity-75">
+      <div className="text-center items-center justify-center flex flex-col">
+        <Link
+          className="flex flex-col justify-between items-center text-sm font-bold"
+          href="https://tools.sandri.my.id"
+          target="_blank"
+        >
+          <Image src="/resolve.webp" alt="elvxk tool" width={23} height={23} />
+          ELVXK TOOL
+        </Link>
+        <p className="text-sm">
+          Made with love © {new Date().getFullYear()}{" "}
+          <Link href="sandri.my.id" target="_blank">
+            elvxk
+          </Link>
+        </p>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
